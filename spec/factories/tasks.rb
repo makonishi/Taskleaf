1a36d13875fe6268e317eb6697d235e81a22250e
Makotox0207 <xiaoxishicheng@gmail.com>
2021-03-19 00:10:19 +0900

    finished

---
+++ spec/factories/tasks.rb
@@ -0,0 +1,8 @@
+factoryBot.define do
+  factory :task do
+    name { 'テストを書く'}
+    description {'RSpec&Capybara&FactoryBotを準備する'}
+    # :userという名前のFactoryをTaskモデルに定義されたuserという名前の関連を生成するのに利用する
+    user
+  end
+end
